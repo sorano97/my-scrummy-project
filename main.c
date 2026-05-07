c67cade0fcacdfeb9fcb1029a81d55c79db0527a
Sorano Tomiyama <soranotomiyama@SoranonoMacBook-Air.local>
2026-05-07 14:24:56 +0900

    コメントを追加

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
+// 基礎プログラミングII 第3回 Scrummyプロジェクト
 #include <SchooMyUtilities.h>
 SchooMyUtilities scmUtils = SchooMyUtilities();
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   pinMode(19, OUTPUT);
   digitalWrite(19, HIGH);
 }
 
-void loop() {
+void loop()
+{
   digitalWrite(19, LOW);
 }
